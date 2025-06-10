33fbc00a84e6a4104f9dd870a637babaafac5137
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-06-09 21:50:05 -0300

    feat(sidebar): Implement render list items recursively

--- src/components/Layouts/sidebar/data/index.ts
+++ src/components/Layouts/sidebar/data/index.ts
@@ -2,381 +2,371 @@ import { ReactElement } from "react";
 
 import * as Icons from "../icons";
 
-interface SidebarSectionItem {
+export interface SidebarItem {
   title: string;
   icon?: (props: Icons.PropsType) => ReactElement;
   url?: string;
-  items?: SidebarSectionItem[];
+  items?: SidebarItem[];
 }
 
-interface SidebarSection {
-  label: string;
-  items: SidebarSectionItem[];
-}
-
-export const NAV_DATA: SidebarSection[] = [
+export const NAV_DATA: SidebarItem[] = [
   {
-    label: "MAIN MENU",
+    title: "Início",
+    url: "/",
+    icon: Icons.HomeIcon,
+    items: [],
+  },
+  {
+    title: "Cadastros",
+    icon: Icons.Forms,
     items: [
       {
-        title: "Início",
-        url: "/",
-        icon: Icons.HomeIcon,
-        items: [],
+        title: "Empregadores",
+        url: "/cadastros/empregadores",
       },
       {
-        title: "Cadastros",
-        icon: Icons.Forms,
+        title: "Unidades",
+        url: "/cadastros/unidades",
+      },
+      {
+        title: "Terceirização",
+        url: "/cadastros/terceiros",
+      },
+      {
+        title: "Setores",
+        url: "/cadastros/setores",
+      },
+      {
+        title: "Funções",
+        url: "/cadastros/funcoes",
+      },
+      {
+        title: "Trabalhadores",
+        url: "/cadastros/trabalhadores",
+      },
+      {
+        title: "Profissionais",
+        url: "/cadastros/profissionais",
+      },
+      {
+        title: "Riscos Ocupacionais",
+        url: "/cadastros/riscos-ocupacionais",
+      },
+
+      {
+        title: "Equipamentos",
+        url: "/cadastros/equipamentos",
+      },
+      {
+        title: "Customização",
         items: [
           {
-            title: "Empregadores",
-            url: "/cadastros/empregadores",
+            title: "Anexos Padrão",
+            url: "/cadastros/customizar/anexos",
           },
           {
-            title: "Unidades",
-            url: "/cadastros/unidades",
+            title: "Textos Padrão",
+            url: "/cadastros/customizar/txt-padrao",
           },
           {
-            title: "Terceirização",
-            url: "/cadastros/terceiros",
+            title: "Anexos Padrão",
+            url: "/cadastros/customizar/txt-rapidos",
           },
           {
-            title: "Setores",
-            url: "/cadastros/setores",
+            title: "Anexos Padrão",
+            url: "/cadastros/customizar/ordem-servico",
+          },
+        ],
+      },
+      {
+        title: "Segurança do Trabalho",
+        items: [
+          {
+            title: "Medidas de Proteção",
+            url: "/cadastros/seguranca-trabalho/medidas",
           },
           {
-            title: "Funções",
-            url: "/cadastros/funcoes",
+            title: "Ambientes de Trabalho",
+            url: "/cadastros/seguranca-trabalho/ambientes",
           },
           {
-            title: "Trabalhadores",
-            url: "/cadastros/trabalhadores",
+            title: "Análise Ergonômica",
+            url: "/cadastros/seguranca-trabalho/analise",
           },
           {
-            title: "Profissionais",
-            url: "/cadastros/profissionais",
+            title: "Avaliações Quantitativas",
+            url: "/cadastros/seguranca-trabalho/avaliacoes",
           },
           {
-            title: "Riscos Ocupacionais",
-            url: "/cadastros/riscos-ocupacionais",
-          },
-
-          {
-            title: "Equipamentos",
-            url: "/cadastros/equipamentos",
+            title: "Treinamentos",
+            url: "/cadastros/seguranca-trabalho/treinamentos",
           },
           {
-            title: "Customização",
+            title: "Documentos",
+            url: "/cadastros/seguranca-trabalho/documentos",
+          },
+          {
+            title: "GHE Riscos",
+            url: "/cadastros/seguranca-trabalho/ghe",
+          },
+          {
+            title: "APR/APP",
             items: [
               {
-                title: "Anexos Padrão",
-                url: "/cadastros/customizar/anexos",
+                title: "Etapas",
+                url: "/cadastros/seguranca-trabalho/apr-app/etapas",
               },
               {
-                title: "Textos Padrão",
-                url: "/cadastros/customizar/txt-padrao",
-              },
-              {
-                title: "Anexos Padrão",
-                url: "/cadastros/customizar/txt-rapidos",
-              },
-              {
-                title: "Anexos Padrão",
-                url: "/cadastros/customizar/ordem-servico",
-              },
-            ],
-          },
-          {
-            title: "Segurança do Trabalho",
-            items: [
-              {
-                title: "Medidas de Proteção",
-                url: "/cadastros/seguranca-trabalho/medidas",
-              },
-              {
-                title: "Ambientes de Trabalho",
-                url: "/cadastros/seguranca-trabalho/ambientes",
-              },
-              {
-                title: "Análise Ergonômica",
-                url: "/cadastros/seguranca-trabalho/analise",
-              },
-              {
-                title: "Avaliações Quantitativas",
-                url: "/cadastros/seguranca-trabalho/avaliacoes",
-              },
-              {
-                title: "Treinamentos",
-                url: "/cadastros/seguranca-trabalho/treinamentos",
-              },
-              {
-                title: "Documentos",
-                url: "/cadastros/seguranca-trabalho/documentos",
-              },
-              {
-                title: "GHE Riscos",
-                url: "/cadastros/seguranca-trabalho/ghe",
+                title: "Riscos x Atividades",
+                url: "/cadastros/seguranca-trabalho/apr-app/riscos",
               },
               {
                 title: "APR/APP",
-                items: [
-                  {
-                    title: "Etapas",
-                    url: "/cadastros/seguranca-trabalho/apr-app/etapas",
-                  },
-                  {
-                    title: "Riscos x Atividades",
-                    url: "/cadastros/seguranca-trabalho/apr-app/riscos",
-                  },
-                  {
-                    title: "APR/APP",
-                    url: "/cadastros/seguranca-trabalho/apr-app/",
-                  },
-                ],
+                url: "/cadastros/seguranca-trabalho/apr-app/",
               },
             ],
+          },
+        ],
+      },
+      {
+        title: "Medicina do Trabalho",
+        items: [
+          {
+            title: "Exames",
+            url: "/cadastros/med-trabalho/exames",
           },
           {
-            title: "Medicina do Trabalho",
-            items: [
-              {
-                title: "Exames",
-                url: "/cadastros/med-trabalho/exames",
-              },
-              {
-                title: "GHE Exames",
-                url: "/cadastros/med-trabalho/ghe-exames",
-              },
-            ],
+            title: "GHE Exames",
+            url: "/cadastros/med-trabalho/ghe-exames",
           },
+        ],
+      },
 
+      {
+        title: "Clínica",
+        items: [
           {
-            title: "Clínica",
-            items: [
-              {
-                title: "Aptidões",
-                url: "/cadastros/clinica/aptidoes",
-              },
-              {
-                title: "Prontuário Clínico",
-                items: [
-                  {
-                    title: "Categorias",
-                    url: "/cadastros/clinica/aptidoes/categorias",
-                  },
-                  {
-                    title: "Modelos",
-                    url: "/cadastros/clinica/aptidoes/modelos",
-                  },
-                ],
-              },
-              {
-                title: "Convênios",
-                url: "/cadastros/clinica/convenios",
-              },
-              {
-                title: "Famílias de Exames",
-                url: "/cadastros/clinica/exames",
-              },
-              {
-                title: "Jornadas",
-                url: "/cadastros/clinica/jornadas",
-              },
-              {
-                title: "Salas",
-                url: "/cadastros/clinica/salas",
-              },
-              {
-                title: "Guichês",
-                url: "/cadastros/clinica/guiches",
-              },
-            ],
+            title: "Aptidões",
+            url: "/cadastros/clinica/aptidoes",
           },
           {
-            title: "Comercial",
-            items: [
-              {
-                title: "Segmentos",
-                url: "/cadastros/comercial/segmentos",
-              },
-              {
-                title: "Contabilidades",
-                url: "/cadastros/comercial/contabilidades",
-              },
-            ],
-          },
-          {
-            title: "Financeiro",
+            title: "Prontuário Clínico",
             items: [
               {
                 title: "Categorias",
-                url: "/cadastros/financeiro/categorias",
+                url: "/cadastros/clinica/aptidoes/categorias",
               },
               {
-                title: "Serviços",
-                url: "/cadastros/financeiro/servicos",
-              },
-              {
-                title: "Condições de Pagamentos",
-                url: "/cadastros/financeiro/pagamentos",
-              },
-              {
-                title: "Fornecedores",
-                url: "/cadastros/financeiro/fornecedores",
-              },
-              {
-                title: "Tabela de Preços",
-                url: "/cadastros/financeiro/tabela-precos",
-              },
-              {
-                title: "Tabela de Custos",
-                url: "/cadastros/financeiro/tabela-custos",
-              },
-              {
-                title: "Caixa / Bancos",
-                url: "/cadastros/financeiro/caixa",
-              },
-              {
-                title: "Centro de Custos",
-                url: "/cadastros/financeiro/centro",
+                title: "Modelos",
+                url: "/cadastros/clinica/aptidoes/modelos",
               },
             ],
           },
           {
-            title: "Estoque",
-            items: [
-              {
-                title: "Estoques",
-                url: "/cadastros/estoque",
-              },
-              {
-                title: "Grupo de Materiais",
-                url: "/cadastros/estoque/grupo",
-              },
-              {
-                title: "Materiais",
-                url: "/cadastros/estoque/materiais",
-              },
-            ],
+            title: "Convênios",
+            url: "/cadastros/clinica/convenios",
+          },
+          {
+            title: "Famílias de Exames",
+            url: "/cadastros/clinica/exames",
+          },
+          {
+            title: "Jornadas",
+            url: "/cadastros/clinica/jornadas",
+          },
+          {
+            title: "Salas",
+            url: "/cadastros/clinica/salas",
+          },
+          {
+            title: "Guichês",
+            url: "/cadastros/clinica/guiches",
           },
         ],
       },
       {
-        title: "Vínculos",
-        url: "/vinculos",
-        icon: Icons.User,
-        items: [],
-      },
-      {
-        title: "Gestão",
-        icon: Icons.Management,
+        title: "Comercial",
         items: [
           {
-            title: "Segurança do Trabalho",
+            title: "Segmentos",
+            url: "/cadastros/comercial/segmentos",
+          },
+          {
+            title: "Contabilidades",
+            url: "/cadastros/comercial/contabilidades",
+          },
+        ],
+      },
+      {
+        title: "Financeiro",
+        items: [
+          {
+            title: "Categorias",
+            url: "/cadastros/financeiro/categorias",
+          },
+          {
+            title: "Serviços",
+            url: "/cadastros/financeiro/servicos",
+          },
+          {
+            title: "Condições de Pagamentos",
+            url: "/cadastros/financeiro/pagamentos",
+          },
+          {
+            title: "Fornecedores",
+            url: "/cadastros/financeiro/fornecedores",
+          },
+          {
+            title: "Tabela de Preços",
+            url: "/cadastros/financeiro/tabela-precos",
+          },
+          {
+            title: "Tabela de Custos",
+            url: "/cadastros/financeiro/tabela-custos",
+          },
+          {
+            title: "Caixa / Bancos",
+            url: "/cadastros/financeiro/caixa",
+          },
+          {
+            title: "Centro de Custos",
+            url: "/cadastros/financeiro/centro",
+          },
+        ],
+      },
+      {
+        title: "Estoque",
+        items: [
+          {
+            title: "Estoques",
+            url: "/cadastros/estoque",
+          },
+          {
+            title: "Grupo de Materiais",
+            url: "/cadastros/estoque/grupo",
+          },
+          {
+            title: "Materiais",
+            url: "/cadastros/estoque/materiais",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Vínculos",
+    url: "/vinculos",
+    icon: Icons.User,
+    items: [],
+  },
+  {
+    title: "Gestão",
+    icon: Icons.Management,
+    items: [
+      {
+        title: "Segurança do Trabalho",
+        items: [
+          {
+            title: "PPPs",
+            url: "/gestao/seguranca/ppps",
+          },
+          {
+            title: "EPIs",
+            url: "/gestao/seguranca/epis",
+          },
+          {
+            title: "Treinamentos",
+            url: "/gestao/seguranca/treinamentos",
+          },
+          {
+            title: "Documentos",
+            url: "/gestao/seguranca/documentos",
+          },
+          {
+            title: "Ordens de Serviço",
+            url: "/gestao/seguranca/ordens",
+          },
+          {
+            title: "CIPA",
             items: [
               {
-                title: "PPPs",
-                url: "/gestao/seguranca/ppps",
+                title: "Gestões",
+                url: "/gestao/seguranca/cipa/gestoes",
               },
               {
-                title: "EPIs",
-                url: "/gestao/seguranca/epis",
-              },
-              {
-                title: "Treinamentos",
-                url: "/gestao/seguranca/treinamentos",
-              },
-              {
-                title: "Documentos",
-                url: "/gestao/seguranca/documentos",
-              },
-              {
-                title: "Ordens de Serviço",
-                url: "/gestao/seguranca/ordens",
-              },
-              {
-                title: "CIPA",
-                items: [
-                  {
-                    title: "Gestões",
-                    url: "/gestao/seguranca/cipa/gestoes",
-                  },
-                  {
-                    title: "Reuniões",
-                    url: "/gestao/seguranca/cipa/reunioes",
-                  },
-                ],
-              },
-              {
-                title: "FDS",
-                url: "/gestao/seguranca/fds",
+                title: "Reuniões",
+                url: "/gestao/seguranca/cipa/reunioes",
               },
             ],
           },
           {
-            title: "Medicina do Trabalho",
-            items: [
-              {
-                title: "Afastamentos",
-                url: "/gestao/medicina/afastamentos",
-              },
-              {
-                title: "Atestados",
-                url: "/gestao/medicina/Atestados",
-              },
-              {
-                title: "Resultados de Exames",
-                url: "/gestao/medicina/resultados-exames",
-              },
-              {
-                title: "Resultados Exames Retroativos",
-                url: "/gestao/medicina/resultados-retroativos",
-              },
-            ],
+            title: "FDS",
+            url: "/gestao/seguranca/fds",
           },
+        ],
+      },
+      {
+        title: "Medicina do Trabalho",
+        items: [
+          {
+            title: "Afastamentos",
+            url: "/gestao/medicina/afastamentos",
+          },
+          {
+            title: "Atestados",
+            url: "/gestao/medicina/Atestados",
+          },
+          {
+            title: "Resultados de Exames",
+            url: "/gestao/medicina/resultados-exames",
+          },
+          {
+            title: "Resultados Exames Retroativos",
+            url: "/gestao/medicina/resultados-retroativos",
+          },
+        ],
+      },
 
-          {
-            title: "Clínica",
-            items: [
-              {
-                title: "ASO Avulso",
-                url: "/gestao/clinica/aso-avulso",
-              },
-              {
-                title: "ASO",
-                url: "/gestao/clinica/aso",
-              },
-            ],
-          },
-        ],
-      },
       {
-        title: "CAT",
-        url: "/cat",
-        icon: Icons.Fire,
-        items: [],
-      },
-      {
-        title: "eSocial",
-        icon: Icons.ESocial,
+        title: "Clínica",
         items: [
           {
-            title: "Certificados",
-            url: "/esocial/certificados/",
+            title: "ASO Avulso",
+            url: "/gestao/clinica/aso-avulso",
           },
           {
-            title: "Painel",
-            url: "/esocial/painel/",
-          },
-          {
-            title: "Eventos",
-            url: "/esocial/eventos/",
-          },
-          {
-            title: "Envios",
-            url: "/esocial/envios/",
+            title: "ASO",
+            url: "/gestao/clinica/aso",
           },
         ],
+      },
+    ],
+  },
+  {
+    title: "CAT",
+    url: "/cat",
+    icon: Icons.Fire,
+    items: [],
+  },
+  {
+    title: "eSocial",
+    icon: Icons.ESocial,
+    items: [
+      {
+        title: "Certificados",
+        url: "/esocial/certificados/",
+      },
+      {
+        title: "Painel",
+        url: "/esocial/painel/",
+      },
+      {
+        title: "Eventos",
+        url: "/esocial/eventos/",
+      },
+      {
+        title: "Envios",
+        url: "/esocial/envios/",
       },
     ],
   },
--- src/components/Layouts/sidebar/index.tsx
+++ src/components/Layouts/sidebar/index.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { NAV_DATA } from "./data";
+import { NAV_DATA, SidebarItem } from "./data";
 import { ArrowLeftIcon, ChevronUp } from "./icons";
 import { MenuItem } from "./menu-item";
 import { useSidebarContext } from "./sidebar-context";
@@ -16,28 +16,81 @@ export function Sidebar() {
   const [expandedItems, setExpandedItems] = useState<string[]>([]);
 
   const toggleExpanded = (title: string) => {
-    setExpandedItems((prev) => (prev.includes(title) ? [] : [title]));
+    setExpandedItems((prev) =>
+      prev.includes(title) ? prev.filter((t) => t !== title) : [...prev, title],
+    );
+  };
 
-    // Uncomment the following line to enable multiple expanded items
-    // setExpandedItems((prev) =>
-    //   prev.includes(title) ? prev.filter((t) => t !== title) : [...prev, title],
-    // );
+  const renderListItem = (item: SidebarItem) => {
+    if (item.items?.length) {
+      return (
+        <div key={item?.url ?? item.title}>
+          <MenuItem
+            isActive={item.items.some(({ url }) => url === pathname)}
+            onClick={() => toggleExpanded(item.title)}
+          >
+            {item.icon && (
+              <item.icon className="size-6 shrink-0" aria-hidden="true" />
+            )}
+
+            <span className="text-left">{item.title}</span>
+
+            <ChevronUp
+              className={cn(
+                "ml-auto rotate-180 transition-transform duration-200",
+                expandedItems.includes(item.title) && "rotate-0",
+              )}
+              aria-hidden="true"
+            />
+          </MenuItem>
+
+          {expandedItems.includes(item.title) && (
+            <ul
+              className="ml-9 mr-0 space-y-1.5 pb-[15px] pr-0 pt-2"
+              role="menu"
+            >
+              {item.items.map((subItem) => renderListItem(subItem))}
+            </ul>
+          )}
+        </div>
+      );
+    }
+
+    const href =
+      "url" in item
+        ? item.url + ""
+        : "/" + item.title.toLowerCase().split(" ").join("-");
+
+    return (
+      <li key={item?.url ?? item.title}>
+        <MenuItem
+          className="flex items-center gap-3 py-3"
+          as="link"
+          href={href}
+          isActive={pathname === href}
+        >
+          {item.icon && (
+            <item.icon className="size-6 shrink-0" aria-hidden="true" />
+          )}
+
+          <span className="text-left">{item.title}</span>
+        </MenuItem>
+      </li>
+    );
   };
 
   useEffect(() => {
     // Keep collapsible open, when it's subpage is active
-    NAV_DATA.some((section) => {
-      return section.items.some((item) => {
-        return item?.items?.some((subItem) => {
-          if (subItem.url === pathname) {
-            if (!expandedItems.includes(item.title)) {
-              toggleExpanded(item.title);
-            }
-
-            // Break the loop
-            return true;
+    NAV_DATA.some((item) => {
+      return item?.items?.some((subItem) => {
+        if (subItem.url === pathname) {
+          if (!expandedItems.includes(item.title)) {
+            toggleExpanded(item.title);
           }
-        });
+
+          // Break the loop
+          return true;
+        }
       });
     });
   }, [pathname]);
@@ -87,95 +140,13 @@ export function Sidebar() {
 
           {/* Navigation */}
           <div className="custom-scrollbar mt-6 flex-1 overflow-y-auto pr-3 min-[850px]:mt-10">
-            {NAV_DATA.map((section) => (
-              <div key={section.label} className="mb-6">
-                <h2 className="mb-5 text-sm font-medium text-dark-4 dark:text-dark-6">
-                  {section.label}
-                </h2>
-
-                <nav role="navigation" aria-label={section.label}>
-                  <ul className="space-y-2">
-                    {section.items.map((item) => (
-                      <li key={item.title}>
-                        {item?.items?.length ? (
-                          <div>
-                            <MenuItem
-                              isActive={item.items.some(
-                                ({ url }) => url === pathname,
-                              )}
-                              onClick={() => toggleExpanded(item.title)}
-                            >
-                              {item.icon && (
-                                <item.icon
-                                  className="size-6 shrink-0"
-                                  aria-hidden="true"
-                                />
-                              )}
-
-                              <span>{item.title}</span>
-
-                              <ChevronUp
-                                className={cn(
-                                  "ml-auto rotate-180 transition-transform duration-200",
-                                  expandedItems.includes(item.title) &&
-                                    "rotate-0",
-                                )}
-                                aria-hidden="true"
-                              />
-                            </MenuItem>
-
-                            {expandedItems.includes(item.title) && (
-                              <ul
-                                className="ml-9 mr-0 space-y-1.5 pb-[15px] pr-0 pt-2"
-                                role="menu"
-                              >
-                                {item.items.map((subItem) => (
-                                  <li key={subItem.title} role="none">
-                                    <MenuItem
-                                      as="link"
-                                      href={subItem.url ?? ""}
-                                      isActive={pathname === subItem.url}
-                                    >
-                                      <span>{subItem.title}</span>
-                                    </MenuItem>
-                                  </li>
-                                ))}
-                              </ul>
-                            )}
-                          </div>
-                        ) : (
-                          (() => {
-                            const href =
-                              "url" in item
-                                ? item.url + ""
-                                : "/" +
-                                  item.title.toLowerCase().split(" ").join("-");
-
-                            return (
-                              <MenuItem
-                                className="flex items-center gap-3 py-3"
-                                as="link"
-                                href={href}
-                                isActive={pathname === href}
-                              >
-                                {item.icon && (
-                                  <item.icon
-                                    className="size-6 shrink-0"
-                                    aria-hidden="true"
-                                  />
-                                )}
-
-                                <span>{item.title}</span>
-                              </MenuItem>
-                            );
-                          })()
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                </nav>
-              </div>
-            ))}
+            <div className="mb-6">
+              <nav role="navigation">
+                <ul className="space-y-2">
+                  {NAV_DATA.map((item) => renderListItem(item))}
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </aside>
