33a74c528a282bc2fcbb379a726f11f1f3aa4752
Gabriel Augusto Fontes da Silva <gabriel_fsilva@outlook.com.br>
2025-06-11 13:41:06 -0300

    fix(logo): Changes text color on dark mode

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
   return (
-    <h1 className="text-2xl font-bold text-dark">
+    <h1 className="text-2xl font-bold text-dark dark:text-white">
       Axion <span className="text-sm font-normal">HSE</span>
     </h1>
   );
