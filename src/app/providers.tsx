01842d8cc6e14ecb91d17c418bdea195a2193c33
Gabriel Augusto Fontes da Silva <gabriel_fsilva@outlook.com.br>
2025-06-17 17:21:59 -0300

    refactor(hooks): Removes use-employers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import { SidebarProvider } from "@/components/Layouts/sidebar/sidebar-context";
-import { EmployerProvider } from "@/hooks/use-employers";
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
-      <SidebarProvider>
-        <EmployerProvider>{children}</EmployerProvider>
-      </SidebarProvider>
+      <SidebarProvider>{children}</SidebarProvider>
     </ThemeProvider>
   );
 }
