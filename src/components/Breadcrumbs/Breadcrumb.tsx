03f4809fa0159a96e7e82ab40fa7bc0c432211cc
Gabriel Augusto Fontes da Silva <gabriel_fsilva@outlook.com.br>
2025-06-30 18:51:15 -0300

    refactor(breadcrumb): Changes how to get page name

--- src/components/Breadcrumbs/Breadcrumb.tsx
+++ src/components/Breadcrumbs/Breadcrumb.tsx
@@ -1,10 +1,31 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { NAV_DATA, SidebarItem } from "../Layouts/sidebar/data";
 
 interface BreadcrumbProps {
   pageName: string;
 }
 
-const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
+const flat =
+  (
+    mainParent?: SidebarItem[],
+  ): ((o: SidebarItem) => SidebarItem[] | SidebarItem) =>
+  (o?: SidebarItem): SidebarItem[] | SidebarItem => {
+    if (!o) return [];
+    return o.items ? o.items.flatMap(flat(mainParent)) : { ...o };
+  };
+
+const Breadcrumb = ({}: BreadcrumbProps) => {
+  const titlesAndUrls = NAV_DATA.flatMap(flat()) as {
+    title: string;
+    url: string;
+  }[];
+
+  const path = usePathname();
+  const pageName = titlesAndUrls.find((item) => item.url === path)?.title;
+
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       <h2 className="text-[26px] font-bold leading-[30px] text-dark dark:text-white">
