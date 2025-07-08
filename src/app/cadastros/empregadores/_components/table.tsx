2ac84d167777e1dca1dfb7393e206717dee0803f
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-07-08 06:49:13 -0300

    feat(empregadores): Adds pagination component

--- src/app/cadastros/empregadores/_components/table.tsx
+++ src/app/cadastros/empregadores/_components/table.tsx
@@ -2,6 +2,7 @@ import Table from "@/components/ui/table";
 import { TrashIcon, PencilSquareIcon } from "@heroicons/react/24/solid";
 import { TableRow } from "@/components/ui/table/row";
 import { TableCell } from "@/components/ui/table/cell";
+import Pagination from "@/components/ui/pagination";
 import { getEmployers } from "../fetch";
 
 const COLUMNS = [
@@ -20,60 +21,66 @@ export default async function EmployersTable() {
   const employers = await getEmployers();
 
   return (
-    <Table
-      rows={employers}
-      cols={COLUMNS}
-      renderRowsMobile={(employer) => (
-        <div
-          key={employer.id}
-          className={`p-4 *:flex *:justify-between ${spanSelector}:font-medium ${spanSelector}:text-gray-900 ${spanSelector}:dark:text-white`}
-        >
-          <div>
-            <span>{COLUMNS[0]}:</span>
-            <span>{employer.id}</span>
-          </div>
-          <div>
-            <span>{COLUMNS[1]}:</span>
-            <span>{employer.razaoSocial}</span>
-          </div>
-          <div>
-            <span>{COLUMNS[2]}:</span>
-            <span>{employer.situacao}</span>
-          </div>
-          <div>
-            <span>{COLUMNS[3]}:</span>
-            <span>{employer.tipo}</span>
-          </div>
-          <div>
-            <span>{COLUMNS[4]}:</span>
-            <span>{employer.cidade}</span>
-          </div>
-          <div>
-            <span>{COLUMNS[5]}:</span>
-            <span>{employer.uf}</span>
-          </div>
-          <div className="mt-2 !justify-end *:w-4">
-            <PencilSquareIcon />
-            <TrashIcon className="ml-2" />
-          </div>
-        </div>
-      )}
-      renderRowsDesktop={(employer) => (
-        <TableRow key={employer.id}>
-          <TableCell>{employer.id}</TableCell>
-          <TableCell>{employer.razaoSocial}</TableCell>
-          <TableCell>{employer.situacao}</TableCell>
-          <TableCell>{employer.tipo}</TableCell>
-          <TableCell>{employer.cidade}</TableCell>
-          <TableCell>{employer.uf}</TableCell>
-          <TableCell>
-            <div className="flex gap-1">
-              <PencilSquareIcon className="w-4" />
-              <TrashIcon className="ml-2 w-4" />
+    <>
+      <Table
+        rows={employers}
+        cols={COLUMNS}
+        renderRowsMobile={(employer) => (
+          <div
+            key={employer.id}
+            className={`p-4 *:flex *:justify-between ${spanSelector}:font-medium ${spanSelector}:text-gray-900 ${spanSelector}:dark:text-white`}
+          >
+            <div>
+              <span>{COLUMNS[0]}:</span>
+              <span>{employer.id}</span>
             </div>
-          </TableCell>
-        </TableRow>
-      )}
-    />
+            <div>
+              <span>{COLUMNS[1]}:</span>
+              <span>{employer.razaoSocial}</span>
+            </div>
+            <div>
+              <span>{COLUMNS[2]}:</span>
+              <span>{employer.situacao}</span>
+            </div>
+            <div>
+              <span>{COLUMNS[3]}:</span>
+              <span>{employer.tipo}</span>
+            </div>
+            <div>
+              <span>{COLUMNS[4]}:</span>
+              <span>{employer.cidade}</span>
+            </div>
+            <div>
+              <span>{COLUMNS[5]}:</span>
+              <span>{employer.uf}</span>
+            </div>
+            <div className="mt-2 !justify-end *:w-4">
+              <PencilSquareIcon />
+              <TrashIcon className="ml-2" />
+            </div>
+          </div>
+        )}
+        renderRowsDesktop={(employer) => (
+          <TableRow key={employer.id}>
+            <TableCell>{employer.id}</TableCell>
+            <TableCell>{employer.razaoSocial}</TableCell>
+            <TableCell>{employer.situacao}</TableCell>
+            <TableCell>{employer.tipo}</TableCell>
+            <TableCell>{employer.cidade}</TableCell>
+            <TableCell>{employer.uf}</TableCell>
+            <TableCell>
+              <div className="flex gap-1">
+                <PencilSquareIcon className="w-4" />
+                <TrashIcon className="ml-2 w-4" />
+              </div>
+            </TableCell>
+          </TableRow>
+        )}
+      />
+
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={employers.length} />
+      </div>
+    </>
   );
 }
---
+++ src/components/ui/pagination/number.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import clsx from "clsx";
+import Link from "next/link";
+
+export default function PaginationNumber({
+  page,
+  href,
+  isActive,
+  position,
+}: {
+  page: number | string;
+  href: string;
+  position?: "first" | "last" | "middle" | "single";
+  isActive: boolean;
+}) {
+  const className = clsx(
+    "flex h-10 w-10 items-center justify-center text-sm border",
+    {
+      "rounded-l-md": position === "first" || position === "single",
+      "rounded-r-md": position === "last" || position === "single",
+      "z-10 bg-primary border-primary text-white": isActive,
+      "hover:bg-gray-100 hover:text-dark hover:dark:bg-[#FFFFFF1A] hover:dark:text-white":
+        !isActive && position !== "middle",
+      "text-gray-300": position === "middle",
+    },
+  );
+
+  return isActive || position === "middle" ? (
+    <div className={className}>{page}</div>
+  ) : (
+    <Link href={href} className={className}>
+      {page}
+    </Link>
+  );
+}
