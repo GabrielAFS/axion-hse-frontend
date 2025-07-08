1923e16919d5208cb0fbe61ed08d9e1b39bfb804
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-07-08 14:00:32 -0300

    feat(empregadores): Adds button to redirect to form

---
+++ src/app/cadastros/empregadores/novo/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewEmployerPage: React.FC = () => {
+  return <div />;
+};
+
+export default NewEmployerPage;
--- src/app/cadastros/empregadores/page.tsx
+++ src/app/cadastros/empregadores/page.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from "next";
 import EmployersTable from "./_components/table";
+import { Button } from "@/components/ui-elements/button";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 const TITLE = "Empregadores";
 
@@ -8,9 +11,30 @@ export const metadata: Metadata = {
 };
 
 const EmployersPage = () => {
+  const formHref = "/cadastros/empregadores/novo";
+
   return (
     <>
+      <div className="flex justify-end">
+        <Link href={formHref} className="hidden md:block">
+          <Button
+            label="Novo"
+            icon={<PlusIcon className="w-5" />}
+            shape="rounded"
+            size="small"
+          />
+        </Link>
+      </div>
       <EmployersTable />
+
+      <Link href={formHref} className="fixed bottom-4 right-4 md:hidden">
+        <Button
+          label=""
+          icon={<PlusIcon className="w-6" />}
+          shape="full"
+          size="small"
+        />
+      </Link>
     </>
   );
 };
