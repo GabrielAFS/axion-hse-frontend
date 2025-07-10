4c09d26e9a8a9f1679fdab17307c53bb991fd22e
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-07-10 17:10:34 -0300

    fix(forms): Breadcrumb props

--- src/app/forms/form-elements/page.tsx
+++ src/app/forms/form-elements/page.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function FormElementsPage() {
   return (
     <>
-      <Breadcrumb pageName="Form Elements" />
+      <Breadcrumb />
 
       <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
         <div className="flex flex-col gap-9">
