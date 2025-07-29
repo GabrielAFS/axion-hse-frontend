f5e1f30670bf061683c179d3f1de88de924e6580
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-07-29 20:44:08 -0300

    feat(empregadores): Creates page for edit informations

---
+++ src/app/cadastros/empregadores/[id]/editar/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import EditEmployerForm from "../../_components/forms/edit-form";
+
+const EditEmployerPage: React.FC = () => {
+  return <EditEmployerForm />;
+};
+
+export default EditEmployerPage;
---
+++ src/app/cadastros/empregadores/_components/forms/edit-form.tsx
@@ -0,0 +1,13 @@
+import DadosCadastraisForm from "./dados-cadastrais";
+import FinanceiroForm from "./financeiro";
+
+export default function EditEmployerForm() {
+  return (
+    <>
+      <DadosCadastraisForm />
+
+      <FinanceiroForm />
+    </>
+  );
+  1;
+}
