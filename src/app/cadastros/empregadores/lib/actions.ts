91a932cebb1b9e1d65144ca5f93ba0e6a77f1787
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-07-29 23:57:09 -0300

    refactor(empregadores): Creates updateEmployer action

--- src/app/cadastros/empregadores/lib/actions.ts
+++ src/app/cadastros/empregadores/lib/actions.ts
@@ -1,33 +1,19 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { NewEmployerData } from "./schemas";
+import { FinanceiroData, NewEmployerData } from "./schemas";
 
-export type State = {
-  errors?: {
-    tipoInscricao?: string[];
-    identificacao?: string[];
-    email?: string[];
-    cep?: string[];
-    endereco?: string[];
-    bairro?: string[];
-    cidade?: string[];
-    estado?: string[];
-    telefone?: string[];
-    whatsapp?: string[];
-    segmento?: string[];
-    cnpj?: string[];
-    cpf?: string[];
-    razaoSocial?: string[];
-    nomeCompleto?: string[];
-    nomeFantasia?: string[];
-    cnpjEsocial?: string[];
-  };
-  message?: string | null;
-};
+type UpdateEmployerData = Partial<NewEmployerData & FinanceiroData>;
 
 export async function createEmployer(data: NewEmployerData) {
   console.log("Creating employer with data:", data);
+  // Simulate an API call
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  revalidatePath("/cadastros/empregadores");
+}
+
+export async function updateEmployer(id: string, data: UpdateEmployerData) {
+  console.log(`Updating employer with ID ${id} and data:`, data);
   // Simulate an API call
   await new Promise((resolve) => setTimeout(resolve, 1000));
   revalidatePath("/cadastros/empregadores");
